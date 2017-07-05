71936dfd9ca981baabb03a93be53387151edd9c4
Arnaud Pasquelin <arnaud.pasquelin@orange.com>
2017-07-05 11:56:00 +0200

    Part 3 - Adding geometry and physics

--- arkit-by-example/Plane.swift
+++ arkit-by-example/Plane.swift
@@ -12,11 +12,21 @@ import ARKit
 class Plane: SCNNode {
     
     var anchor: ARPlaneAnchor
-    var planeGeometry: SCNPlane
+    var planeGeometry: SCNBox
     
-    init(anchor: ARPlaneAnchor) {
+    init(anchor: ARPlaneAnchor, isHidden hidden: Bool) {
         self.anchor = anchor
-        self.planeGeometry = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
+        let width = CGFloat(self.anchor.extent.x)
+        let length = CGFloat(self.anchor.extent.z)
+        
+        // Using a SCNBox and not SCNPlane to make it easy for the geometry we add to the
+        // scene to interact with the plane.
+        
+        // For the physics engine to work properly give the plane some height so we get interactions
+        // between the plane and the gometry we add to the scene
+        let planeHeight: CGFloat = 0.01
+        
+        self.planeGeometry = SCNBox(width: width, height: planeHeight, length: length, chamferRadius: 0)
         
         super.init()
         
@@ -25,14 +35,25 @@ class Plane: SCNNode {
         let material = SCNMaterial()
         let image = UIImage(named: "tron_grid.png")
         material.diffuse.contents = image
-        self.planeGeometry.materials = [material]
+        
+        // Since we are using a cube, we only want to render the tron grid
+        // on the top face, make the other sides transparent
+        let transparentMaterial = SCNMaterial()
+        transparentMaterial.diffuse.contents = UIColor(white: 1, alpha: 0)
+        
+        if hidden {
+            self.planeGeometry.materials = [transparentMaterial, transparentMaterial, transparentMaterial, transparentMaterial, transparentMaterial, transparentMaterial]
+        } else {
+            self.planeGeometry.materials = [transparentMaterial, transparentMaterial, transparentMaterial, transparentMaterial, material, transparentMaterial]
+        }
         
         let planeNode = SCNNode(geometry: planeGeometry)
-        planeNode.position = SCNVector3Make(anchor.center.x, 0, anchor.center.z)
         
-        // Planes in SceneKit are vertical by default so we need to rotate 90degrees to match
-        // planes in ARKit
-        planeNode.transform = SCNMatrix4MakeRotation(-.pi / 2.0, 1.0, 0.0, 0.0)
+        // Since our plane has some height, move it down to be at the actual surface
+        planeNode.position = SCNVector3Make(0, Float(-planeHeight / 2), 0)
+        
+        // Give the plane a physics body so that items we add to the scene interact with it
+        planeNode.physicsBody = SCNPhysicsBody(type: SCNPhysicsBodyType.kinematic, shape: SCNPhysicsShape(geometry: self.planeGeometry, options: nil))
         
         self.setTextureScale()
         self.addChildNode(planeNode)
@@ -47,28 +68,37 @@ class Plane: SCNNode {
         // may be updated. We need to update our 3D geometry to match the
         // new parameters of the plane.
         self.planeGeometry.width = CGFloat(anchor.extent.x)
-        self.planeGeometry.height = CGFloat(anchor.extent.z)
+        self.planeGeometry.length = CGFloat(anchor.extent.z)
         
         // When the plane is first created it's center is 0,0,0 and the nodes
         // transform contains the translation parameters. As the plane is updated
         // the planes translation remains the same but it's center is updated so
         // we need to update the 3D geometry position
         self.position = SCNVector3Make(anchor.center.x, 0, anchor.center.z)
+        
+        let node = self.childNodes.first
+        node?.physicsBody = SCNPhysicsBody(type: SCNPhysicsBodyType.kinematic, shape: SCNPhysicsShape(geometry: self.planeGeometry, options: nil))
         setTextureScale()
     }
     
     func setTextureScale() {
         let width = planeGeometry.width
-        let height = planeGeometry.height
+        let height = planeGeometry.length
         
         // As the width/height of the plane updates, we want our tron grid material to
         // cover the entire plane, repeating the texture over and over. Also if the
         // grid is less than 1 unit, we don't want to squash the texture to fit, so
         // scaling updates the texture co-ordinates to crop the texture in that case
-        let material = planeGeometry.materials.first
-        material!.diffuse.contentsTransform = SCNMatrix4MakeScale(Float(width), Float(height), 1)
-        material!.diffuse.wrapS = SCNWrapMode.repeat
-        material!.diffuse.wrapT = SCNWrapMode.repeat
+        let material = planeGeometry.materials[4]
+        material.diffuse.contentsTransform = SCNMatrix4MakeScale(Float(width), Float(height), 1)
+        material.diffuse.wrapS = SCNWrapMode.repeat
+        material.diffuse.wrapT = SCNWrapMode.repeat
+    }
+    
+    func hide() {
+        let transparentMaterial = SCNMaterial()
+        transparentMaterial.diffuse.contents = UIColor(white: 1, alpha: 0)
+        self.planeGeometry.materials = [transparentMaterial, transparentMaterial, transparentMaterial, transparentMaterial, transparentMaterial, transparentMaterial]
     }
 }
 
--- arkit-by-example/ViewController.swift
+++ arkit-by-example/ViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+struct CollisionCategory: OptionSet {
+    let rawValue: Int
+    
+    static let bottom = CollisionCategory(rawValue: 1 << 0)
+    static let cube = CollisionCategory(rawValue: 1 << 1)
+}
+
+class ViewController: UIViewController, ARSCNViewDelegate, UIGestureRecognizerDelegate, SCNPhysicsContactDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
     
     // A dictionary of all the current planes being rendered in the scene
     var planes: [UUID:Plane] = [:]
+    // Contains a list of all the boxes rendered in the scene
+    var boxes: [SCNNode] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupScene()
+        self.setupRecognizers()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -56,16 +66,183 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let scene = SCNScene()
         // Set the scene to the view
         self.sceneView.scene = scene
+        
+        // For our physics interactions, we place a large node a couple of meters below the world
+        // origin, after an explosion, if the geometry we added has fallen onto this surface which
+        // is place way below all of the surfaces we would have detected via ARKit then we consider
+        // this geometry to have fallen out of the world and remove it
+        let bottomPlane = SCNBox(width: 1000, height: 0.5, length: 1000, chamferRadius: 0)
+        let bottomMaterial = SCNMaterial()
+        bottomMaterial.diffuse.contents = UIColor(white: 1.0, alpha: 0)
+        bottomPlane.materials = [bottomMaterial]
+        let bottomNode = SCNNode(geometry: bottomPlane)
+        bottomNode.position = SCNVector3Make(0, -10, 0)
+        bottomNode.physicsBody = SCNPhysicsBody(type: SCNPhysicsBodyType.kinematic, shape: nil)
+        bottomNode.physicsBody?.categoryBitMask = CollisionCategory.bottom.rawValue
+        bottomNode.physicsBody?.contactTestBitMask = CollisionCategory.cube.rawValue
+        self.sceneView.scene.rootNode.addChildNode(bottomNode)
+        self.sceneView.scene.physicsWorld.contactDelegate = self
     }
     
     func setupSession() {
         // Create a session configuration
         let configuration = ARWorldTrackingSessionConfiguration()
         
+        // Specify that we do want to track horizontal planes. Setting this will cause the ARSCNViewDelegate
+        // methods to be called when scenes are detected
         configuration.planeDetection = .horizontal
         
         // Run the view's session
         sceneView.session.run(configuration)
+    }
+    
+    func setupRecognizers() {
+        // Single tap will insert a new piece of geometry into the scene
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTapFrom))
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        
+        // Press and hold will cause an explosion causing geometry in the local vicinity of the explosion to move
+        let explosionGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleHoldFrom))
+        explosionGestureRecognizer.minimumPressDuration = 0.5
+        self.sceneView.addGestureRecognizer(explosionGestureRecognizer)
+        
+        let hidePlanesGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleHidePlaneFrom))
+        hidePlanesGestureRecognizer.minimumPressDuration = 1
+        hidePlanesGestureRecognizer.numberOfTouchesRequired = 2
+        self.sceneView.addGestureRecognizer(hidePlanesGestureRecognizer)
+    }
+    
+    @objc func handleTapFrom(recognizer: UITapGestureRecognizer) {
+        // Take the screen space tap coordinates and pass them to the hitTest method on the ARSCNView instance
+        let tapPoint = recognizer.location(in: self.sceneView)
+        let result = self.sceneView.hitTest(tapPoint, types: ARHitTestResult.ResultType.existingPlaneUsingExtent)
+        
+        // If the intersection ray passes through any plane geometry they will be returned, with the planes
+        // ordered by distance from the camera
+        if result.count == 0 {
+            return
+        }
+        
+        // If there are multiple hits, just pick the closest plane
+        let hitResult = result.first
+        self.insertGeometry(hitResult: hitResult!)
+    }
+    
+    @objc func handleHoldFrom(recognizer: UILongPressGestureRecognizer) {
+        if recognizer.state != UIGestureRecognizerState.began {
+            return
+        }
+        
+        // Perform a hit test using the screen coordinates to see if the user pressed on
+        // a plane.
+        let holdPoint = recognizer.location(in: self.sceneView)
+        let result = self.sceneView.hitTest(holdPoint, types: ARHitTestResult.ResultType.existingPlaneUsingExtent)
+        if result.count == 0 {
+            return
+        }
+        
+        let hitResult = result.first
+        DispatchQueue.main.async(execute: {
+            self.explode(hitResult: hitResult!)
+        })
+    }
+    
+    @objc func handleHidePlaneFrom(recognizer: UILongPressGestureRecognizer) {
+        if recognizer.state != UIGestureRecognizerState.began {
+            return
+        }
+        
+        // Hide all the planes
+        for (planeID, _) in self.planes {
+            self.planes[planeID]?.hide()
+        }
+        
+        // Stop detecting new planes or updating existing ones.
+        let configuration = self.sceneView.session.configuration as! ARWorldTrackingSessionConfiguration
+        configuration.planeDetection = ARWorldTrackingSessionConfiguration.PlaneDetection(rawValue: 0)
+        self.sceneView.session.run(configuration)
+    }
+    
+    func explode(hitResult: ARHitTestResult) {
+        // For an explosion, we take the world position of the explosion and the position of each piece of geometry
+        // in the world. We then take the distance between those two points, the closer to the explosion point the
+        // geometry is the stronger the force of the explosion.
+        
+        // The hitResult will be a point on the plane, we move the explosion down a little bit below the
+        // plane so that the goemetry fly upwards off the plane
+        let explosionYOffset: Float = 0.1
+        
+        let position = SCNVector3Make(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y.advanced(by: -explosionYOffset), hitResult.worldTransform.columns.3.z)
+        
+        // We need to find all of the geometry affected by the explosion, ideally we would have some
+        // spatial data structure like an octree to efficiently find all geometry close to the explosion
+        // but since we don't have many items, we can just loop through all of the current geometry
+        for cubeNode in self.boxes {
+            // The distance between the explosion and the geometry
+            var distance = SCNVector3Make(cubeNode.worldPosition.x - position.x, cubeNode.worldPosition.y - position.y, cubeNode.worldPosition.z - position.z)
+            let length: Float = sqrtf(distance.x * distance.x + distance.y * distance.y + distance.z * distance.z)
+            
+            // Set the maximum distance that the explosion will be felt, anything further than 2 meters from
+            // the explosion will not be affected by any forces
+            let maxDistance: Float = 2
+            var scale = max(0, maxDistance - length)
+            
+            // Scale the force of the explosion
+            scale = scale * scale * 2
+            
+            // Scale the distance vector to the appropriate scale
+            distance.x = distance.x / length * scale
+            distance.y = distance.y / length * scale
+            distance.z = distance.z / length * scale
+            
+            // Apply a force to the geometry. We apply the force at one of the corners of the cube
+            // to make it spin more, vs just at the center
+            cubeNode.physicsBody?.applyForce(distance, at: SCNVector3Make(0.05, 0.05, 0.05), asImpulse: true)
+        }
+    }
+    
+    func insertGeometry(hitResult: ARHitTestResult) {
+        // Right now we just insert a simple cube, later we will improve these to be more
+        // interesting and have better texture and shading
+        let dimension: CGFloat = 0.1
+        let cube = SCNBox(width: dimension, height: dimension, length: dimension, chamferRadius: 0)
+        let node = SCNNode(geometry: cube)
+        
+        // The physicsBody tells SceneKit this geometry should be manipulated by the physics engine
+        node.physicsBody = SCNPhysicsBody(type: SCNPhysicsBodyType.dynamic, shape: nil)
+        node.physicsBody?.mass = 2.0
+        node.physicsBody?.categoryBitMask = CollisionCategory.cube.rawValue
+        
+        // We insert the geometry slightly above the point the user tapped, so that it drops onto the plane
+        // using the physics engine
+        let insertionYOffset: Float = 0.5
+        node.position = SCNVector3Make(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y.advanced(by: insertionYOffset), hitResult.worldTransform.columns.3.z)
+        self.sceneView.scene.rootNode.addChildNode(node)
+        self.boxes.append(node)
+    }
+    
+    // MARK: - SCNPhysicsContactDelegate
+    
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        // Here we detect a collision between pieces of geometry in the world, if one of the pieces
+        // of geometry is the bottom plane it means the geometry has fallen out of the world. just remove it
+        guard let physicsBodyA = contact.nodeA.physicsBody, let physicsBodyB = contact.nodeB.physicsBody else {
+            return
+        }
+        
+        let categoryA = CollisionCategory.init(rawValue: physicsBodyA.categoryBitMask)
+        let categoryB = CollisionCategory.init(rawValue: physicsBodyB.categoryBitMask)
+        
+        let contactMask: CollisionCategory? = [categoryA, categoryB]
+        
+        if contactMask == [CollisionCategory.bottom, CollisionCategory.cube] {
+            if categoryA == CollisionCategory.bottom {
+                contact.nodeB.removeFromParentNode()
+            } else {
+                contact.nodeA.removeFromParentNode()
+            }
+        }
     }
     
     // MARK: - ARSCNViewDelegate
@@ -76,7 +253,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
         
         // When a new plane is detected we create a new SceneKit plane to visualize it in 3D
-        let plane = Plane(anchor: anchor as! ARPlaneAnchor)
+        let plane = Plane(anchor: anchor as! ARPlaneAnchor, isHidden: false)
         planes[anchor.identifier] = plane
         node.addChildNode(plane)
     }
